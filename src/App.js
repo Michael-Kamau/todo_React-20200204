e80b6beaf0a5eac8b743be72803ef804545cce22
Michael-Kamau <kamau.karitu@gmail.com>
2020-02-04 13:43:23 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Todos from "./components/Todos";
+import Header from "./components/layouts/Header";
+import AddTodo from "./components/AddTodo";
+import uuid from 'uuid'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    //Toggle todo completed
+    markComplete = (id) => {
+        console.log('I am in the App.js')
+        this.setState({
+            todos: this.state.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo;
+            })
+        });
+    }
+//Delete todo item(returning todos that don't match the passed in id
+    delTodo = (id) => {
+        this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)]})
+    }
+
+    //Add Todo
+    addTodo=(title)=>{
+        const newTodo={
+            id:uuid.v4(),
+            title,
+            completed: false
+        }
+        this.setState({todos:[...this.state.todos,newTodo]})
+    }
+    state = {
+        todos: [
+            {
+                id: uuid.v4(),
+                title: 'Details of the todo',
+                completed: false
+            },
+            {
+                id: uuid.v4(),
+                title: 'Going to the market',
+                completed: false
+            },
+            {
+                id: uuid.v4(),
+                title: 'Reading a novel',
+                completed: false
+            }
+        ]
+    }
+
+    render() {
+        console.log(this.state.todos)
+        return (
+            <div className="App">
+                <div className="container">
+                    <Header/>
+                    <AddTodo addTodo={this.addTodo}/>
+                    <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
