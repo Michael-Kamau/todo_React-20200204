8af7fcb2a1152c1471cb9993f38cf0748ffc9824
Michael-Kamau <kamau.karitu@gmail.com>
2020-02-06 02:00:39 +0300

    Updates

---
+++ precache-manifest.8a15eabdcd9769cc4ee3ca34cdfb6b07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72c35b08f1b0eba8385d6bdf92315471",
+    "url": "/todo_React-20200204/index.html"
+  },
+  {
+    "revision": "0ba3ee5edef1e36ab490",
+    "url": "/todo_React-20200204/static/css/main.f9f6dbcc.chunk.css"
+  },
+  {
+    "revision": "3b78be432f47032846e9",
+    "url": "/todo_React-20200204/static/js/2.e7f8c452.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todo_React-20200204/static/js/2.e7f8c452.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ba3ee5edef1e36ab490",
+    "url": "/todo_React-20200204/static/js/main.a4edb995.chunk.js"
+  },
+  {
+    "revision": "76cd1899bf1ab7fa2a69",
+    "url": "/todo_React-20200204/static/js/runtime-main.8e6415e3.js"
+  }
+]);
